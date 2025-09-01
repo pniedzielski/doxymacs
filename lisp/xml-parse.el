0c2d91b04f5d0e9f7df961d5de4075b2721aa145
Patrick M. Niedzielski <patrick@pniedzielski.net>
2025-09-01 19:09:46 +0000

    fix: Add `lexical-binding` directives
    This patch adds a `lexical-binding` directive to each of the package’s
    elisp files, disabling lexical binding.  This makes the current binding
    behavior explicit and silences the following warnings when
    byte-compiling:
    
        Warning: file has no ‘lexical-binding’ directive on its first line
    
    This patch does not enable lexical binding.  We should verify that the
    current code works with lexical binding (it likely does) before enabling
    it.
    
    License: GPLv3+
    Signed-off-by: Patrick M. Niedzielski <patrick@pniedzielski.net>

--- lisp/xml-parse.el
+++ lisp/xml-parse.el
@@ -1,4 +1,4 @@
-;;; xml-parse --- code to efficiently read/write XML data with Elisp
+;;; xml-parse --- code to efficiently read/write XML data with Elisp   -*- mode: emacs-lisp; lexical-binding: nil -*-
 
 ;; Copyright (C) 2001 John Wiegley.
 
